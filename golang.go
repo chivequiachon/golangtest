367d779205652ec3ae1ab5cd2c0b063ff199a445
chivequiachon <chivequiachon@gmail.com>
2020-11-26 01:47:14 +0800

    Added package

--- golang.go
+++ golang.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chivequiachon/golangtest/stringtests"
+)
 
 func main() {
-	fmt.Println("Hello GoLang!")
+	fmt.Println(stringtests.ReverseRunes("!1gnaloG ,olleH"))
 }
